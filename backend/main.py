f383d782fae80c8f2c24aa017cd622f865e2fa7a
Aditya Sreekar Poluri <sreekarp@amazon.com>
2026-03-05 21:42:36 +0000

    fixed some uvicorn stuff

--- backend/main.py
+++ backend/main.py
@@ -174,4 +174,4 @@ if __name__ == "__main__":
         level="INFO",
     )
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
